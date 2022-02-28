cd4adbfbe2e02bb4e8e1f9955f705607da6d5132
Adnan Nassra <mohamad-adnan.nassar@adesso.de>
2022-02-28 22:04:38 +0100

    added: test msg

--- src/AlteKlausurenVonHiba/Klausur_2019_2020_Marz/Aufgabe_8/UploaderApp.java
+++ src/AlteKlausurenVonHiba/Klausur_2019_2020_Marz/Aufgabe_8/UploaderApp.java
@@ -2,6 +2,7 @@ package AlteKlausurenVonHiba.Klausur_2019_2020_Marz.Aufgabe_8;
 
 public class UploaderApp {
     public static void main(String[] args) {
+        System.out.println("HALLO");
         String name = args[0];
         UploadVerifier verifier = new JavaFileVerifier();
         Uploadable file = new SimpleFile(name);
